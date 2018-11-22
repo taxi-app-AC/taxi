3c8ae2f7e70a6b0e1078560a7fb5719ff2b1eb7e
Abbas <abbaszeynalov070@gmail.com>
2018-11-22 17:51:07 +0400

    order change

--- controllers/OrderController.js
+++ controllers/OrderController.js
@@ -4,8 +4,30 @@ var Order = require('../models/Order');
 
 exports.create = function (req, res) {
 
-        Order.a();
-        // Order.insertOrder(req, res);
+        // Order.a();
+
+
+    // let orderObj = {
+    //     currently_place:{
+    //         lon:req.body.currently_place.lon,
+    //         lat:req.body.currently_place.lat
+    //     },
+    //     where_to:{
+    //         lon: req.body.where_to.lon,
+    //         lat: req.body.where_to.lat
+    //     },
+    //     km: req.body.km,
+    //     amount: req.body.amount,
+    //     transmitter: req.body.transmitter,
+    //     phone: req.body.phone,
+    //     start_time: req.body.start_time,
+    //     end_time: req.body.end_time,
+    // };
+
+
+        // Order.insertOrder(orderObj);
+
+
 
     // post gondermey ucun
 
@@ -27,35 +49,31 @@ exports.create = function (req, res) {
     // }
 
 
-    // let orderObj = {
-    //     currently_place:{
-    //         lon:req.body.currently_place.lon,
-    //         lat:req.body.currently_place.lat
-    //     },
-    //     where_to:{
-    //         lon: req.body.where_to.lon,
-    //         lat: req.body.where_to.lat
-    //     },
-    //     km: req.body.km,
-    //     amount: req.body.amount,
-    //     transmitter: req.body.transmitter,
-    //     phone: req.body.phone,
-    //     start_time: req.body.start_time,
-    //     end_time: req.body.end_time,
-    // };
-    //
-    // Order.create(orderObj,
-    //     function (err) {
-    //         if (err) {
-    //
-    //             return res.status(500).send("There was a problem create the Order.")
-    //         }
-    //
-    //         return res.status(200).send({ Order: 'success' });
-    //     });
+    let orderObj = {
+        currently_place:{
+            lon:req.body.currently_place.lon,
+            lat:req.body.currently_place.lat
+        },
+        where_to:{
+            lon: req.body.where_to.lon,
+            lat: req.body.where_to.lat
+        },
+        km: req.body.km,
+        amount: req.body.amount,
+        transmitter: req.body.transmitter,
+        phone: req.body.phone,
+        start_time: req.body.start_time,
+        end_time: req.body.end_time,
+    };
 
-        // Order.insertData(req,res) =function (req,res) {
-        //
-        //     };
-        // res.send({msg:'abbas'})
+    Order.create(orderObj,
+        function (err) {
+            if (err) {
+
+                return res.status(500).send("There was a problem create the Order.")
+            }
+
+            return res.status(200).send({ Order: 'success' });
+        });
+
 };
