5ab9c0085aaccc9a9e0424ec6e4a63ed97af920c
cavid2409 <cavid2409@gmail.com>
2018-12-08 19:35:39 +0400

    npm

--- test/test.js
+++ test/test.js
@@ -1,9 +1,8 @@
 var assert = require('assert');
-describe('Basic Mocha String Test', function () {
-    it('should return number of charachters in a string', function () {
-        assert.equal("Hello".length, 4);
-    });
-    it('should return first charachter of the string', function () {
-        assert.equal("Hello".charAt(0), 'H');
+describe('Array', function() {
+    describe('#indexOf()', function() {
+        it('should return -1 when the value is not present', function() {
+            assert.equal([1,2,3].indexOf(4), -1);
+        });
     });
 });
